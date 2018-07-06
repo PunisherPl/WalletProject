3558176525e7c7a7a0dfdae10f5bed48accaae5e
PunisherPL <thewolok@gmail.com>
2018-07-06 21:09:18 +0200

    Calculate

---
+++ src/Calculate.java
@@ -0,0 +1,29 @@
+
+public class Calculate {
+
+	
+	Calculate()
+	{
+		
+	}
+	
+	
+	public double money(double expensive, double income)
+	{
+		double value =  income - expensive;
+		return value;
+	}
+	public double expenses (double expensive, double expensivve)
+	{ double value;
+	
+		value = expensive + expensivve;
+		
+		return value;
+	}
+	public double expensess (double expensive)
+	{
+		double value = expensive;
+		
+		return value;
+	}
+}
